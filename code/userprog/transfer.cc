342113395b4c77ac072e8b7f7d5d040a71b28f06
SebastianMoline <sebastianmoline@gmail.com>
2019-04-28 16:16:38 -0300

    Práctica 3 Ejercicio 1. Not tested.

--- code/userprog/transfer.cc
+++ code/userprog/transfer.cc
@@ -3,6 +3,22 @@
 #include "threads/system.hh"
 
 
+/// Copy a byte array from virtual machine to host.
+void ReadBufferFromUser(int userAddress, char *outBuffer,
+                        unsigned byteCount)
+{
+    ASSERT(userAddress != 0);
+    ASSERT(outBuffer != nullptr);
+    ASSERT(byteCount != 0);
+    
+    for(unsigned count = 0; count < byteCount; count++, userAddress++){
+        int temp;
+        ASSERT(machine->ReadMem(userAddress, 1, &temp));
+        *outBuffer = (unsigned char) temp;
+    }
+}
+
+/// Copy a C string from virtual machine to host.
 bool ReadStringFromUser(int userAddress, char *outString,
                         unsigned maxByteCount)
 {
@@ -21,4 +37,24 @@ bool ReadStringFromUser(int userAddress, char *outString,
     return *(outString - 1) == '\0';
 }
 
-// TODO: complete...
+/// Copy a byte array from host to virtual machine.
+void WriteBufferToUser(const char *buffer, int userAddress,
+                       unsigned byteCount)
+{
+    ASSERT(userAddress != 0);
+    ASSERT(buffer != nullptr);
+    ASSERT(byteCount != 0);
+    
+    for(unsigned count = 0; count < byteCount; count++, userAddress++, buffer++)
+        ASSERT(machine->WriteMem(userAddress, 1, *buffer));
+}
+
+/// Copy a C string from host to virtual machine.
+void WriteStringToUser(const char *string, int userAddress){
+    ASSERT(userAddress != 0);
+    ASSERT(string != nullptr);
+    
+    do {
+        ASSERT(machine->WriteMem(userAddress++, 1, *string));
+    } while (*string++ != '\0');
+}
