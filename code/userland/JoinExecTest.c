9cdc992a040c2d743c3ce24d3c8fb6f2d6c29b91
Sebastián Moliné <38443011+SebastianMoline@users.noreply.github.com>
2019-05-09 15:36:07 -0300

    Upload test cases
    Testing Exec and Join syscalls

---
+++ code/userland/JoinExecTest.c
@@ -0,0 +1,29 @@
+/// Simple program to test whether running a user program works.
+///
+/// Just do a “syscall” that shuts down the OS.
+///
+/// NOTE: for some reason, user programs with global data structures
+/// sometimes have not worked in the Nachos environment.  So be careful out
+/// there!  One option is to allocate data structures as automatics within a
+/// procedure, but if you do this, you have to be careful to allocate a big
+/// enough stack to hold the automatics!
+
+#include "syscall.h"
+
+int
+main(void)
+{
+    OpenFileId o = Open("../userland/test.txt");
+    
+    Join(Exec(../userland/AuxTest));
+
+    char aux[64];
+    Read(aux, 64, o);
+    Write(aux, 64, CONSOLE_OUTPUT);
+
+
+
+    // Hopefully reached.
+    Write("This should be printed.\n", 28, CONSOLE_OUTPUT);
+    Halt();
+}
