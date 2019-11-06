afb7bdb3b54f779d64f62aa813489f92273b2456
SebastianMoline <sebastianmoline@gmail.com>
2019-11-06 16:58:05 -0300

    Finished implementing File Path. Tested and working.

--- code/filesys/file_path.cc
+++ code/filesys/file_path.cc
@@ -1,117 +1,49 @@
 #include "file_path.hh"
 
-
-// Creates a file path from a string that describes it, 
+// Creates a file path from a string that describes it,
 // or sets it to an empty path if NULL is given.
 FilePath::FilePath(char *pathString){
-	if(pathString == nullptr)
-		first = last = nullptr;
-	else{
-		first = new PathNode;
-		first -> name[0] = '\0';
-		first -> prev = nullptr;
-		
-		PathNode *previousNode = first;
-		
-		for(unsigned idx = 1; pathString[idx] != '\0'; idx ++){
-			char buffer[FILE_NAME_MAX_LEN + 2];
-			unsigned bufferIdx = 0;
-			while(pathString[idx] != '/' and pathString[idx] != '\0'){
-				buffer[bufferIdx] = pathString[idx];
-				bufferIdx ++;
-				idx ++;
-			}
-			
-			buffer[bufferIdx] = '\0';
-			previousNode -> next = new PathNode;
-			previousNode -> next -> prev = previousNode;
-			strcpy(previousNode -> next -> name, buffer);
-			previousNode = previousNode -> next;
-			
-			if(pathString[idx] == '\0')
-				idx --;
-			
-		}
-		
-		last = previousNode;
-		last -> next = nullptr;		
-	}
+	first = last = nullptr;
+	if(pathString != nullptr and *pathString != '\0' and strcmp(pathString, "/"))
+		MergeString(pathString);
 }
-
 
 FilePath::~FilePath(){
-	while(first != nullptr){
-		first = first -> next;
-		free(first -> prev);
-	}
-}
-
-// Given a string for a root, appends the current path after it.
-void
-FilePath::Prepend(char *root){
-	PathNode *aux = new PathNode;
-	strcpy(aux -> name, root);
-	aux -> next = first;
-	aux -> prev = nullptr;
-	
-	if(IsEmpty())
-		first = last = aux;
-	else{
-		first -> prev = aux;
-		first = aux;	
-	}
-}
-
-// Appends a given path to the current one.
-void
-FilePath::Append(char *leaf){
-	PathNode *aux = new PathNode;
-	strcpy(aux -> name, leaf);
-	aux -> prev = last;
-	aux -> next = nullptr;
-	
-	if(IsEmpty())
-		first = last = aux;
-	else{
-		last -> next = aux;
-		last = aux;
-	}
+	Clear();
 }
 
 // Returns a string describing the current path.
 char *
 FilePath::ToString(){
-	if(IsEmpty())
-		return nullptr;
-	
+	if(IsEmpty()){
+		char * result = new char[2];
+		strcpy(result, "/");
+		return result;
+	}
+
 	PathNode *aux = first;
 	unsigned counter = 0;
-	
+
 	while(aux != nullptr){
 		aux = aux -> next;
 		counter ++;
 	}
-	
-	char *result = new char[FILE_NAME_MAX_LEN * counter + counter];
+
+	char *result = new char[FILE_NAME_MAX_LEN * counter + counter + 1];
 	result[0] = '\0';
-	
+
 	aux = first;
 	while(aux != nullptr){
-		strcat(result, aux -> name);
 		strcat(result, "/");
+		strcat(result, aux -> name);
 		aux = aux -> next;
 	}
-	
-	unsigned length = strlen(result);
-	if(length > 1){
-		result[length - 1] = '\0';
-	}
-	
+
 	return result;
 }
 
 // Returns whether the current path is a terminal node.
-bool 
+bool
 FilePath::IsBottomLevel(){
 	if(IsEmpty())
 		return false;
@@ -119,7 +51,7 @@ FilePath::IsBottomLevel(){
 }
 
 // Returns whether the path is empty.
-bool 
+bool
 FilePath::IsEmpty(){
 	return first == nullptr;
 }
@@ -130,22 +62,98 @@ char *
 FilePath::SplitBottomLevel(){
 	if(IsEmpty())
 		return nullptr;
-	
+
 	char *result = new char[FILE_NAME_MAX_LEN + 1];
-	
+
 	strcpy(result, first -> name);
-	
+
 	PathNode *aux = first;
 	if(first == last)
 		first = last = nullptr;
 	else
 		first = first -> next;
-	
+
 	delete aux;
 	return result;
 }
 
-// Given a string describing a path, appends it
-// to the current path.
-void 
-FilePath::Merge(char *pathString){}
+// Given a string describing a path,
+// - if the string represents a global path, sets the current path to it.
+// - if not, appends the path described by the string to the current path.
+void
+FilePath::Merge(char *pathString){
+	if(*pathString == '/')
+		Clear();
+
+	MergeString(pathString);
+}
+
+// Resets the path to a null path.
+void
+FilePath::Clear(){
+	PathNode *aux;
+	while(first != nullptr){
+		aux = first;
+		first = first -> next;
+		delete aux;
+	}
+	first = last = nullptr;
+}
+
+// Appends the path described by the given string to the current path.
+void
+FilePath::MergeString(char *pathString){
+	if(*pathString == '/')
+		pathString ++;
+
+	PathNode *previousNode = last;
+
+	for(unsigned idx = 0; pathString[idx] != '\0'; idx ++){
+		char buffer[FILE_NAME_MAX_LEN + 1];
+		unsigned bufferIdx = 0;
+		while(pathString[idx] != '/' and pathString[idx] != '\0'){
+			buffer[bufferIdx] = pathString[idx];
+			bufferIdx ++;
+			idx ++;
+		}
+		buffer[bufferIdx] = '\0';
+
+		if(strcmp(buffer, ".")){
+			if(strcmp(buffer, "..")){
+				if(previousNode == nullptr){
+					first = new PathNode;
+					first -> prev = nullptr;
+					strcpy(first -> name, buffer);
+					previousNode = first;
+				}
+				else{
+					previousNode -> next = new PathNode;
+					previousNode -> next -> prev = previousNode;
+					strcpy(previousNode -> next -> name, buffer);
+					previousNode = previousNode -> next;
+				}
+			}
+			else{
+				if(first != nullptr){
+					if(previousNode == first){
+						delete first;
+						previousNode = first = nullptr;
+					}
+					else{
+						previousNode = previousNode -> prev;
+						delete previousNode -> next;
+					}
+				}
+			}
+
+		}
+
+		if(pathString[idx] == '\0')
+			idx --;
+	}
+
+	last = previousNode;
+	if(last != nullptr)
+		last -> next = nullptr;
+
+}
--- code/filesys/file_path.hh
+++ code/filesys/file_path.hh
@@ -3,23 +3,17 @@
 
 #include "open_file.hh"
 
-
 struct PathNode{
 	char name[FILENAME_NAME_MAX_LEN];
 	PathNode *next, *prev;
 };
 
-
 class FilePath{
 public:
-	// Creates a file path from a string that describes it, 
+	// Creates a file path from a string that describes it,
 	// or sets it to an empty path if NULL is given.
 	FilePath(char *pathString);
 	~FilePath();
-	// Given a string for a root, appends the current path after it.
-	void Prepend(char *root);
-	// Appends a given path to the current one.
-	void Append(char *leaf);
 	// Returns a string describing the current path.
 	char *ToString();
 	// Returns whether the current path is a terminal node.
@@ -29,11 +23,16 @@ public:
 	// Splits the root node, returning it as a string
 	// and removing it from the current path.
 	char *SplitBottomLevel();
-	// Given a string describing a path, appends it
-	// to the current path.
+	// Given a string describing a path,
+	// - if the string represents a global path, sets the current path to it.
+	// - if not, appends the path described by the string to the current path.
 	void Merge(char *pathString);
-
+	// Resets the path to a null path.
+	void Clear();
 private:
+	// Appends the path described by the given string to the current path.
+	void MergeString(char *pathString);
+
 	PathNode *first, *last;
 };
 #endif
